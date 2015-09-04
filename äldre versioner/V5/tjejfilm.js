f19e2df2b4475a7914b916ad4755ae5af94505b8
Elvira Boman <you@example.com>
2015-09-04 22:17:37 +0200

    first upload

---
+++ äldre versioner/V5/tjejfilm.js
@@ -0,0 +1,43 @@
+function filmBildPorrskadat(text) {        {
+    var display = document.getElementById('text-display');
+    display.innerHTML = "";
+    display.innerHTML = text;
+}
+
+
+function filmBildPorrskadat20(x) {
+    x.style.height = "100px";
+    x.style.width = "64px";
+}
+
+
+function filmBildKHIOM(x) {
+    x.style.height = "100px";
+    x.style.width = "64px";
+}
+
+
+function filmBildOnesize(x) {
+    x.style.height = "100px";
+    x.style.width = "64px";
+}
+
+
+function filmBildNor(x) {
+    x.style.width = "200px";
+}
+
+
+/*			<p><img class="thumb" src="Porrskadat.png" onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0">
+				<a class="titel">Porrskadat</a> är en intervjufilm där åtta tjejer berättar om hur porr påverkat deras relationer, sexualitet och självbild. Det är personliga berättelser som problematiserar kring skapad sexualitet och vad ett allt mer pornofierat samhälle kan göra med ens varande. 
+				</br><br/><br/>
+			<img class="thumb" src="porrskadat2.png" onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0">
+				<a class="titel">Porrskadat 2.0</a> är en intervjufilm med liknande uppbyggnad, men med män istället för kvinnor som berättar om hur deras relation till porr ser ut och hur den påverkar dem. 
+				</br><br/></br></br>
+			<img class="thumb" src="serny.png" onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0">
+				<a class="titel">Kön handlar inte om kön</a> är en dokumentärfilm där tvåkönsnormen ifrågasätts bland annat genom att vi får höra fyra peroners upplevelser av att inte identifiera sig med ett linjärt kön.
+				</br><br/><br/><br/>
+			<img class="thumb" src="Onesize.png" onmouseover="bigImg(this)" onmouseout="normalImg(this)" border="0">
+				<a class="titel">Onesize</a> är en kortfilm som handlar om dynamiken i en vänskap där vännerna befinner sig på två sidor av skönhetsidealet.
+				</br></br>
+				</p>
